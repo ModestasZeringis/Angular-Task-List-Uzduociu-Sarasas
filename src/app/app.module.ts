d23e92edea98f33da0813cbdea89ff988b2d61af
Modestas <61542037+ModestasZeringis@users.noreply.github.com>
2022-10-25 18:23:57 +0300

    .

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { TaskDetailsComponent } from './components/task-details/task-details.component';
+import { EditTaskComponent } from './components/edit-task/edit-task.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import {FormsModule} from "@angular/forms";
+import { TaskItemComponent } from './components/tasks/task-item/task-item.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes:Routes=[
+  {path:"",component:TasksComponent},
+  {path:"new", component:AddTaskComponent},
+  {path:"edit/:id", component:EditTaskComponent}
+  ]
+@NgModule({
+  declarations: [
+    AppComponent,
+    TasksComponent,
+    AddTaskComponent,
+    TaskDetailsComponent,
+    EditTaskComponent,
+    NavigationComponent,
+    TaskItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
